a9e76de2778e2debeed2ae9d0ca42740784374a8
alyxvkeniii <168285415+alyxvkeniii@users.noreply.github.com>
2025-03-30 01:49:23 +0800

    fix services

--- resources/views/frontend/pages/cart.blade.php
+++ resources/views/frontend/pages/cart.blade.php
@@ -162,8 +162,8 @@
 					<!-- Start Single Service -->
 					<div class="single-service">
 						<i class="ti-rocket"></i>
-						<h4>Free shiping</h4>
-						<p>Orders over $100</p>
+						<h4>Free shipping</h4>
+						<p>Orders over P500</p>
 					</div>
 					<!-- End Single Service -->
 				</div>
@@ -180,7 +180,7 @@
 					<!-- Start Single Service -->
 					<div class="single-service">
 						<i class="ti-lock"></i>
-						<h4>Sucure Payment</h4>
+						<h4>Secure Payment</h4>
 						<p>100% secure payment</p>
 					</div>
 					<!-- End Single Service -->
@@ -189,7 +189,7 @@
 					<!-- Start Single Service -->
 					<div class="single-service">
 						<i class="ti-tag"></i>
-						<h4>Best Peice</h4>
+						<h4>Best Piece</h4>
 						<p>Guaranteed price</p>
 					</div>
 					<!-- End Single Service -->
